37f3c62cd21be00c0af34250d30dcf78a63d562c
marinarolla <averyanova.m@gmail.com>
2021-08-07 21:12:04 +0300

    ligic of useful function refactorinng

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,10 @@
 import prompt
 
 
+def set_username():
+    return prompt.string('May I have your name? ')
+
+
 def welcome_user():
-    user_name = prompt.string('May I have your name? ')
+    user_name = set_username()
     print('Hello, {}!'.format(user_name))
