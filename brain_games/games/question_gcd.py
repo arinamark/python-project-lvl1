48e874f0ad03a9610ad85c337f0a2bcfcd399f92
marinarolla <averyanova.m@gmail.com>
2021-08-08 23:38:40 +0300

    lint fix

--- brain_games/games/question_gcd.py
+++ brain_games/games/question_gcd.py
@@ -2,6 +2,7 @@ from random import randint
 from math import gcd
 from brain_games.games.questions import ask_question, get_answer
 
+
 def gen_expression():
     a = randint(0, 100)
     b = randint(0, 100)
