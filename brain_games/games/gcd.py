6945450d688e8fc623f12b1cf9ca25f995e75e59
marinarolla <averyanova.m@gmail.com>
2021-08-16 20:37:12 +0300

    brain-gcd refactoring

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -3,9 +3,12 @@ from random import randint
 from math import gcd
 
 
+GEN_GCD = 50
+
+
 def gen_expression():
-    a = randint(0, 50)
-    b = randint(0, 50)
+    a = randint(0, GEN_GCD)
+    b = randint(0, GEN_GCD)
     result = gcd(a, b)
     string = '{} {}'.format(str(a), str(b))
     return (result, string)
