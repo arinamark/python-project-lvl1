b4acda83fde48f74a594a61da68f8dcfd974e5e4
marinarolla <averyanova.m@gmail.com>
2021-08-16 21:05:01 +0300

    engine refactoring

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -3,56 +3,48 @@ from brain_games.cli import welcome_user
 import prompt
 
 
-def get_answer():
-    return prompt.string('Your answer: ').rstrip().lower()
-
-
-def ask_question(st):
-    print('Question: {}'.format(st))
+ANSWER_LABEL = 'Your answer: '
+QUESTION_LABEL = 'Question:'
+WIN_LABEL = 'Congratulations'
+CORRECT_LABEL = 'Correct!'
+WRONG_TEXT = ' is wrong answer ;(. Correct answer was '
+TRY_TEXT = 'Let\'s try again'
+WIN_STEP = 3
 
 
 def gen_questions(func):
     (right, string) = func()
-    ask_question(string)
-    answer = get_answer()
+    print('{} {}'.format(QUESTION_LABEL, string))
+    answer = prompt.string(ANSWER_LABEL).rstrip().lower()
     return (str(answer), str(right))
-
-
-def show_win(name):
-    print('Congratulations, {}!'.format(name))
-
-
-def show_correct_stage():
-    print('Correct!')
-
+  
 
 def show_loose(strings):
     (answers, name) = strings
-    center_part = ' is wrong answer ;(. Correct answer was '
-    st = '"{}"{}"{}".'.format(answers[0], center_part, answers[1])
+    st = '"{}"{}"{}".'.format(answers[0], WRONG_TEXT, answers[1])
     print(st)
-    print('Let\'s try again, {}!'.format(name))
+    print('{}, {}!'.format(TRY_TEXT, name))
 
 
 def stage_game(params):
     (answer, right, name, step) = params
     if (answer == right):
-        if(step == 3):
-            show_win(name)
-            return 4
+        if(step == WIN_STEP):
+            print('{}, {}!'.format(WIN_LABEL, name))
+            return WIN_STEP + 1
         else:
-            show_correct_stage()
+            print(CORRECT_LABEL)
             return step + 1
     else:
         params2 = ((answer, right), name)
         show_loose(params2)
-        return 4
+        return WIN_STEP + 1
 
 
 def stages_logic(strings):
     (name, game) = strings
     step = 1
-    while(step <= 3):
+    while(step <= WIN_STEP):
         (answer, right) = gen_questions(game)
         relations = (answer, right, name, step)
         step = stage_game(relations)
