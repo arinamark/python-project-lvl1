77216b4685a14954a69f93e1a85b1af17b289c3c
marinarolla <averyanova.m@gmail.com>
2021-08-28 19:28:10 +0300

    calc game refactoring

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -6,25 +6,39 @@ MAX_INT_FOR_RANDOM = 20
 rule = 'What is the result of the expression?'
 
 
-def get_result(params):
-    (a, b, exp) = params
-    if(exp == '+'):
-        string = '{} + {}'.format(str(a), str(b))
-        result = a + b
-    if(exp == '-'):
-        arg1 = max(a, b)
-        arg2 = min(a, b)
-        string = '{} - {}'.format(str(arg1), str(arg2))
-        result = arg1 - arg2
-    if(exp == '*'):
-        string = '{} * {}'.format(str(a), str(b))
-        result = a * b
-    return (result, string)
-
-
 def gen_expression():
     a = randint(0, MAX_INT_FOR_RANDOM)
     b = randint(0, MAX_INT_FOR_RANDOM)
-    exp = ('+', '-', '*')[randint(0, 2)]
-    params = (a, b, exp)
-    return get_result(params)
+    operator = ('+', '-', '*')[randint(0, 2)]
+    expression = (a, b, operator)
+    return get_correct_answer(expression)
+
+
+def get_correct_answer(expression):
+    (a, b, operator) = expression
+    if(operator == '+'):
+        return add(a, b)
+    if(operator == '-'):
+        argument1 = max(a, b)
+        argument2 = min(a, b)
+        return dev(argument1, argument2)
+    if(operator == '*'):
+        return mul(a, b)
+
+
+def add(a, b):
+    string = '{} + {}'.format(str(a), str(b))
+    result = a + b
+    return (result, string)
+
+
+def dev(a, b):
+    string = '{} - {}'.format(str(a), str(b))
+    result = a - b
+    return (result, string)
+
+
+def mul(a, b):
+    string = '{} * {}'.format(str(a), str(b))
+    result = a * b
+    return (result, string)
