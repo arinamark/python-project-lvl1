b4ac85adf231e10c2fb521eed693fa5022dcc5ba
marinarolla <averyanova.m@gmail.com>
2021-08-28 20:48:46 +0300

    engine refactoring

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -3,51 +3,12 @@ from brain_games.cli import welcome_user
 import prompt
 
 
-ANSWER_LABEL = 'Your answer: '
-QUESTION_LABEL = 'Question:'
-WIN_LABEL = 'Congratulations'
-CORRECT_LABEL = 'Correct!'
-WRONG_TEXT = ' is wrong answer ;(. Correct answer was '
-TRY_TEXT = 'Let\'s try again'
 WIN_STEP = 3
 
 
-def gen_questions(func):
-    (right, string) = func()
-    print('{} {}'.format(QUESTION_LABEL, string))
-    answer = prompt.string(ANSWER_LABEL).rstrip().lower()
-    return (str(answer), str(right))
-
-
-def show_loose(strings):
-    (answers, name) = strings
-    st = '"{}"{}"{}".'.format(answers[0], WRONG_TEXT, answers[1])
-    print(st)
-    print('{}, {}!'.format(TRY_TEXT, name))
-
-
-def stage_game(params):
-    (answer, right, name, step) = params
-    if (answer == right):
-        if(step == WIN_STEP):
-            print('{}, {}!'.format(WIN_LABEL, name))
-            return WIN_STEP + 1
-        else:
-            print(CORRECT_LABEL)
-            return step + 1
-    else:
-        params2 = ((answer, right), name)
-        show_loose(params2)
-        return WIN_STEP + 1
-
-
-def stages_logic(strings):
-    (name, game) = strings
-    step = 1
-    while(step <= WIN_STEP):
-        (answer, right) = gen_questions(game)
-        relations = (answer, right, name, step)
-        step = stage_game(relations)
+def brain_start(rule, game_function):
+    name = start_game(rule)
+    game(name, game_function)
 
 
 def start_game(rule):
@@ -56,7 +17,34 @@ def start_game(rule):
     return name
 
 
-def brain_start(rule, func):
-    name = start_game(rule)
-    relations = (name, func)
-    stages_logic(relations)
+def game(name, game_function):
+    step = 1
+    while(step <= WIN_STEP):
+        (answer, right) = show_question(game_function)
+        step = game_step(answer, right, name, step)
+
+
+def show_question(game_function):
+    (right, expression) = game_function()
+    print('Question: {}'.format(expression))
+    answer = prompt.string('Your answer: ').rstrip().lower()
+    return (str(answer), str(right))
+
+
+def game_step(answer, right, name, step):
+    if (answer == right):
+        if(step == WIN_STEP):
+            print('Congratulations, {}!'.format(name))
+            return WIN_STEP + 1
+        else:
+            print('Correct!')
+            return step + 1
+    else:
+        show_loose(answer, right, name)
+        return WIN_STEP + 1
+
+
+def show_loose(wrong, right, name):
+    st = '{} is wrong answer ;(. Correct answer was {}.'.format(wrong, right)
+    print(st)
+    print('Let\'s try again, {}!'.format(name))
