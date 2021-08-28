25bccf78b8db0cd71ed7dde76de5c16a293ee1b2
marinarolla <averyanova.m@gmail.com>
2021-08-28 18:47:13 +0300

    cli refactoring

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,17 +2,8 @@
 import prompt
 
 
-NAME_ASK_LABEL = 'May I have your name? '
-WELCOME_LABEL = 'Hello'
-START_TEXT = 'Welcome to the Brain Games!'
-
-
-def set_username():
-    return prompt.string(NAME_ASK_LABEL)
-
-
 def welcome_user():
-    print(START_TEXT)
-    user_name = set_username()
-    print('{}, {}!'.format(WELCOME_LABEL, user_name))
+    print('Welcome to the Brain Games!')
+    user_name = prompt.string('May I have your name? ')
+    print('{}, {}!'.format('Hello', user_name))
     return user_name
