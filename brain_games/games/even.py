cad398e0271562eb04f3e940f82ed8462cf6a949
marinarolla <averyanova.m@gmail.com>
2021-09-09 11:36:39 +0300

    even refactoring

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -8,8 +8,8 @@ rule = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 def gen_game_step():
     number = randint(1, MAX_NUMBER_FOR_RANDOM)
-    if(number % 2 == 0):
+    if number % 2 == 0:
         answer = 'yes'
     else:
         answer = 'no'
-    return (answer, str(number))
+    return answer, str(number)
