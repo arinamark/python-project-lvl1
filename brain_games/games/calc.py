7ee354797fdcbd3b2c0b5976123e4adf298706ed
marinarolla <averyanova.m@gmail.com>
2021-09-09 11:54:46 +0300

    calc refactoring

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,5 +1,6 @@
 #!env python
-from random import randint
+from random import randint, choice
+from operator import add, mul, sub
 
 
 MAX_INT_FOR_RANDOM = 20
@@ -9,36 +10,19 @@ rule = 'What is the result of the expression?'
 def gen_game_step():
     a = randint(0, MAX_INT_FOR_RANDOM)
     b = randint(0, MAX_INT_FOR_RANDOM)
-    operator = ('+', '-', '*')[randint(0, 2)]
-    expression = (a, b, operator)
+    operator = choice(['+', '-', '*'])
+    expression = a, b, operator
     return get_step(expression)
 
 
 def get_step(expression):
     a, b, operator = expression
     if operator == '+':
-        return get_step_add(a, b)
+        operation = add
     if operator == '-':
-        argument1 = max(a, b)
-        argument2 = min(a, b)
-        return get_step_sub(argument1, argument2)
+        a = max(a, b)
+        b = min(a, b)
+        operation = sub
     if operator == '*':
-        return get_step_mul(a, b)
-
-
-def get_step_add(a, b):
-    expression = '{} + {}'.format(str(a), str(b))
-    answer = a + b
-    return str(answer), expression
-
-
-def get_step_sub(a, b):
-    expression = '{} - {}'.format(str(a), str(b))
-    answer = a - b
-    return str(answer), expression
-
-
-def get_step_mul(a, b):
-    expression = '{} * {}'.format(str(a), str(b))
-    answer = a * b
-    return str(answer), expression
+        operation = mul
+    return str(operation(a, b)), '{} {} {}'.format(a, operator, b)
